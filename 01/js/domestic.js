37230a2db43d6485227e6e0d564bb1599ccf2264
ahboa <122829723@qq.com>
2019-11-30 00:33:56 +0800

    Delete

--- 01/js/domestic.js
+++
@@ -1,1 +0,0 @@
-document.documentElement.style.fontSize=document.documentElement.clientWidth/7.5+"px";window.onresize=function(){document.documentElement.style.fontSize=document.documentElement.clientWidth/7.5+"px";};
